3d3dc998888a69dbe96da8ea4a952c95c50587fe
allan <allan.hans68350@gmail.com>
2022-06-30 17:07:26 +0200

    agency

--- migrations/Version20220630095547.php
+++ migrations/Version20220630095547.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220630080244 extends AbstractMigration
+final class Version20220630095547 extends AbstractMigration
 {
     public function getDescription(): string
     {
