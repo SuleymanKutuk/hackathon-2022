a6dd519567910154fd29969d2a00b0ea886e4216
SULEYMAN KUTUK <kutuk.suleymann@gmail.com>
2022-06-30 16:17:22 +0200

    load

---
+++ .history/src/Form/ProjectType_20220630143438.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-cntrol']
+            ])
+            ->add(
+                'createdOn',
+                DateType::class,
+                [
+                    'attr' => ['class' => 'form-cntrol']
+                ]
+            )
+            ->add(
+                'status',
+                TextType::class,
+                [
+                    'attr' => ['class' => 'form-cntrol']
+                ]
+            )
+            /*         ->add('deadline',DateType::class,[
+                'attr'=>['class'=>'date']])
+            ->add('agency',null, ['choice_label' => 'name',
+                'attr' => ['class' => 'form-control']])
+            ->add('projectType',null, ['choice_label' => 'type',
+            'attr' => ['class' => 'form-control']])
+            ->add('workSpace',null, ['choice_label' => 'name',
+            'attr' => ['class' => 'form-control']])*/;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
